89dfb528193ff9842da8ff7006623da1df0d8f6d
Silke Bonnen <silke.bonnen@gmail.com>
2022-09-02 17:53:34 +0200

    first commit

---
+++ Asgmt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Assignment0._2;
+public class Program {
+    public static void Main(string[] args) {
+
+    }
+
+    public bool IsLeapYear(int year) {
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) return true;
+                else return false;
+            }
+            else return true;
+        }
+        else return false;
+    }
+}
