64938686cb7666f21b697c18dbb22920238b3fcc
Silke Bonnen <silke.bonnen@gmail.com>
2022-09-06 13:22:53 +0200

    Created user interface for IsLeapYear

--- Asgmt.Tests/UnitTest1.cs
+++ Asgmt.Tests/UnitTest1.cs
@@ -6,10 +6,13 @@ public class UnitTest1
     [Fact]
     public void Eight_is_divisible_by_four()
     {
+        // arrange
         var sut = new Program();
 
+        // act
         var result = sut.IsLeapYear(8);
 
+        // assert
         result.Should().Be(true);
     }
 
@@ -51,5 +54,25 @@ public class UnitTest1
         var result = sut.IsLeapYear(1700);
 
         result.Should().Be(false);
+    }
+
+    [Fact]
+    public void if_leapyear_output_yay()
+    {
+        var sut = new Program();
+
+        var result = sut.yayOrNay(true);
+
+        result.Should().Be("yay");
+    }
+
+    [Fact]
+    public void if_not_leapyear_output_nay()
+    {
+        var sut = new Program();
+
+        var result = sut.yayOrNay(false);
+
+        result.Should().Be("nay");
     }
 }
--- Asgmt/Program.cs
+++ Asgmt/Program.cs
@@ -4,10 +4,15 @@ namespace Asgmt;
 public class Program {
     public static void Main(string[] args) {
 
+        var program = new Program();
+        var input = Console.ReadLine();
+        var year = Int32.Parse(input);
+        var leapYear = program.IsLeapYear(year);
+        var yayornay = program.yayOrNay(leapYear);
+        Console.WriteLine(yayornay);
     }
 
     public bool IsLeapYear(int year) {
-
         if (year % 4 == 0) {
             if (year % 100 == 0) {
                 if (year % 400 == 0) return true;
@@ -16,5 +21,10 @@ public class Program {
             else return true;
         }
         else return false;
+    }
+
+    public string yayOrNay(bool ans) {
+        if (ans == true) return "yay";
+        else return "nay";
     }
 }
